55de9dc2acc13e4c022a7ea2b7b7f4a3316302e1
joaoparaujocr <araujo.joao.paulo.jaa@gmail.com>
2023-05-25 18:20:12 -0400

    fix app test e2e

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './../src/modules/user/user.module';
-import { dataSourceOptions } from 'db/data-source';
+import { dataSourceOptions } from './../db/data-source';
 import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
--- src/modules/auth/auth.guard.ts
+++ src/modules/auth/auth.guard.ts
@@ -7,7 +7,7 @@ import {
 import { Reflector } from '@nestjs/core';
 import { JwtService } from '@nestjs/jwt';
 import { Request } from 'express';
-import { IS_PUBLIC_KEY } from 'src/decorators/publicRoutes.decorator';
+import { IS_PUBLIC_KEY } from './../../decorators/publicRoutes.decorator';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
